679faf5fc00b5059e9658e73f2330720c62d5a36
zaira <zaira00.sand@gmail.com>
2025-08-03 22:03:15 -0600

    correcciones

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Italiana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const italiana = Italiana({
+  weight: ['400', '400'],
+  subsets: ['latin'],
+});
